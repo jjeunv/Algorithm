ee72ef5ad15c0b4b3a1490f1baa254e037b97663
jjeunv <jjeunv@gmail.com>
2023-02-07 19:51:33 +0900

    remove

---
+++ 10834_벨트.py
@@ -0,0 +1,45 @@
+M = int(input()) # 벨트의 개수
+direction = 0 # 바퀴의 회전 방향
+rpm = 0 # 분당 회전수
+
+dirArray = [0, 1] * 501
+index = 0
+
+first, second, belt = map(int,input().split())
+
+if belt == 0 :
+        direction = dirArray[index]
+else :
+        direction = dirArray[index+1]
+        index+=1
+
+
+for i in range(M-1) :
+    a, b, c = map(int, input().split())
+    if a == second :
+        rpm = b
+  
+        first = a 
+        second = b     
+        print(rpm) 
+
+    else :
+        b = b * (second // a) 
+        a = second
+        print(b)
+        rpm = b
+        
+        first = a 
+        second = b
+        print(rpm)
+
+
+    if c == 0 :
+        direction = dirArray[index]
+    else :
+        direction = dirArray[index+1]
+        index+=1
+
+print (direction, rpm)
+
+    

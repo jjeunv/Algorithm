3442082813f298a63aaf634722cd9ca1b37a282a
jjeunv <jjeunv@gmail.com>
2023-02-11 20:34:29 +0900

    solve

--- 2669_직사각형네개의합집합의면적구하기.py
+++ 2669_직사각형네개의합집합의면적구하기.py
@@ -1,4 +1,19 @@
-for i in range(4) :
-    num1,num2,num3,num4 : input().split()
+array = [[0 for j in range(100)] for i in range(100)]
 
-print(num1)
+for i in range(4):
+    num1,num2,num3,num4 = map(int, input().split())
+    num2 = 99-num2 
+    num4 = 99-num4   
+    for j in range(num1, num3) :
+        for k in range(num4+1, num2+1):
+            array[k][j] = 1
+
+count = 0
+
+for i in array :
+    for j in i :
+        if j == 1 :
+            count += 1
+    
+
+print(count)

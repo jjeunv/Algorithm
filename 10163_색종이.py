fc6694beb2cf3ce8ebe1dba522d19373939c0ac3
jjeunv <jjeunv@gmail.com>
2023-02-13 20:55:50 +0900

    solve

--- 10163_색종이.py
+++ 10163_색종이.py
@@ -1,23 +1,18 @@
-N = int(input())
-paper = [[0 for _ in range(10)] for _ in range(10)]
+N = int(input()) #색종이의 장수 
 
-for i in range(1,N +1 ) :
-    x, y, width, height = map (int, input().split())
-    for j in range(x, x+ width) :
-        for k in range(y, y+ height) :
-            paper[j][k] = i
+squre = [[0 for j in range(10)] for i in range (10)]
 
-for i in paper :
-    for j in i:
-        print(j, end= ' ')
-    print()        
+for i in range(1, N + 1) :
+    x, y, w, h = map(int,input().split())
+    for j in range(y, y + h) : 
+        squre[j][x:x+w] = [i] * w
+     
+arr = [0] * (N + 1)
 
-arrCount = [0] * N
+for k in squre :
+    for j in k :
+        arr[j] += 1
 
-for j in range(N) :
-    for i in range(10) :
-        number = paper[i][0:]
-        arrCount[j] += number.count(j)
-
-for i in range(N) :
-    print(arrCount[i], '\n')
+for i in range(1, N+1) :
+    print(arr[i])
+        

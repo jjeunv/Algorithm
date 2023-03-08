63e2a9467702396d3857d850dedbaa11780b1303
jjeunv <jjeunv@gmail.com>
2023-03-08 20:00:49 +0900

    solve

--- 2559_수열.py
+++ 2559_수열.py
@@ -1,19 +1,13 @@
 N, k =map(int,input().split())
 arr=list(map(int,input().split()))
 
-maximum = int(-10000000000000000000)
-index =int(0)
+maxList= []
+maxList.append(sum(arr[:k]))
+
 
 for j in range(N-1):
-    sum = int(0)
-    index =j
-    if j + k -1 <= N-1:
-        
-        for i in range(k) :
-            sum = sum + arr[index]
-            index +=1
-        if sum>maximum :
-            maximum = sum
+    if j + k -1 < N-1:
+        maxList.append(maxList[j]-arr[j]+arr[j+k])
 
 
-print(maximum)
+print(max(maxList))
